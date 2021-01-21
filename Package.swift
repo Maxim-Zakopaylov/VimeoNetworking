4a9f187879bd9e5b8bc1c9bdc170a2c0b2ee6ab4
Maxim Zakopaylov <maxim.zakopaylov@gmail.com>
2021-01-21 19:06:53 +0300

    fix path

--- Package.swift
+++ Package.swift
@@ -20,10 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "VimeoNetworking",
-            path: "Sources",
-            dependencies: []),
-        .testTarget(
-            name: "VimeoNetworkingTests",
-            dependencies: ["VimeoNetworking"]),
+            path: "Sources/Shared"),
     ]
 )
